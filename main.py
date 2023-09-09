52a239a847dd80b836ecbf36f0239a7c84a05617
krylis <cmcshann@gmail.com>
2023-09-09 11:35:38 -0700

    added ability for user to fire a single projectile by pressing the space bar. projectile is destroyed after going off-screen. user cannot fire another projectile until the current projectile is destroyed.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from ship import Ship
+from projectile_manager import ProjectileManager
 import time
 
 screen = Screen()
@@ -9,13 +10,21 @@ screen.title("Space Invaders")
 screen.tracer(0)
 
 player = Ship()
+manager = ProjectileManager(player)
+
 screen.listen()
 screen.onkey(player.move_left, "Left")
 screen.onkey(player.move_right, "Right")
+screen.onkey(manager.create_projectile, "space")
 
 is_game_on = True
 while is_game_on:
     time.sleep(0.1)
     screen.update()
+
+    '''moves player-created projectile across the screen once the user has
+    fired using the space bar'''
+    if manager.projectile:
+        manager.move_projectile()
 
 screen.exitonclick()
